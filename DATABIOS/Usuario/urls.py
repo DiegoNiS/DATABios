a45611313914469f21a70e084b9e650ac5a5d9cf
Diego Nina <dninas@unsa.edu.pe>
2024-06-18 15:35:54 -0500

    ya ahora si

--- DATABIOS/Usuario/urls.py
+++ DATABIOS/Usuario/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import loginView, homeView, logout, list_usuarios, agregar_usuario, inicio_sesion, cerrar_sesion
 
 urlpatterns = [
-    path('login/', inicio_sesion, name='login'),
+    path('', inicio_sesion, name='login'),
     path('usuarios', list_usuarios, name='lista_usuarios'),
     path('agregar/', agregar_usuario, name='agregar_usuario'),
     path('logout/', cerrar_sesion, name='logout'),
