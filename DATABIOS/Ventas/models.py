c625ec2689166ec0abbbdc49bc7111007d5469f0
Ower Frank <olopeza@unsa.edu.pe>
2024-06-20 02:04:29 -0500

    Creacion de Ventas

---
+++ DATABIOS/Ventas/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+
+# Create your models here.
+class Categoria(models.Model):
+    nombre = models.CharField(max_length=100)
+    descripcion = models.TextField(max_length=500)
+
+    def __str__(self):
+        return self.nombre
+
+class Producto(models.Model):
+    categorias = models.ManyToManyField(Categoria)
+    nombre = models.CharField(max_length=100)
+    stock = models.IntegerField()
+    precio_compra = models.FloatField()
+    precio_venta = models.FloatField()
+
+    def __str__(self):
+        return self.nombre
