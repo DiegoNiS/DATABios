9b0496cbccf1453ac67e80cde3e2f9a9d00c0b72
Ower Frank <olopeza@unsa.edu.pe>
2024-06-20 12:02:18 -0500

    Definicion de las vistas

--- DATABIOS/Ventas/views.py
+++ DATABIOS/Ventas/views.py
@@ -1,3 +1,30 @@
-from django.shortcuts import render
+# ventas/views.py
+from django.shortcuts import render, redirect, get_object_or_404
+from django.http import HttpResponse
+from .models import Venta, Vendedor
+from .forms import VentaForm
 
-# Create your views here.
+def ventas_list(request):
+    ventas = Venta.objects.all()
+    return render(request, 'ventas/ventas_list.html', {'ventas': ventas})
+
+def venta_create(request):
+    if request.method == 'POST':
+        form = VentaForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('ventas_list')
+    else:
+        form = VentaForm()
+    return render(request, 'ventas/venta_form.html', {'form': form})
+
+def venta_delete(request, pk):
+    venta = get_object_or_404(Venta, pk=pk)
+    if request.method == 'POST':
+        venta.delete()
+        return redirect('ventas_list')
+    return render(request, 'ventas/venta_confirm_delete.html', {'venta': venta})
+
+def export_to_excel(request):
+    # Aquí iría la lógica para exportar a Excel
+    return HttpResponse('Función para exportar a Excel aún no implementada')
