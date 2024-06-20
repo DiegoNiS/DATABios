eeda2972f88a6d408b02ec8bad58ecd090a6569a
Ower Frank <olopeza@unsa.edu.pe>
2024-06-20 12:01:40 -0500

    Creacion del modelo y el formulario

--- DATABIOS/DATABIOS/settings.py
+++ DATABIOS/DATABIOS/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'Usuario',
     'Inventario',
     'Core',
+    'Ventas',
 ]
 
 MIDDLEWARE = [
--- DATABIOS/Ventas/models.py
+++ DATABIOS/Ventas/models.py
@@ -1,3 +1,17 @@
+# ventas/models.py
 from django.db import models
 
-# Create your models here.
+class Vendedor(models.Model):
+    nombre = models.CharField(max_length=255)
+    apellidos = models.CharField(max_length=255)
+
+    def __str__(self):
+        return f"{self.nombre} {self.apellidos}"
+
+class Venta(models.Model):
+    descripcion = models.TextField()
+    vendedor = models.ForeignKey(Vendedor, on_delete=models.CASCADE)
+    fecha_creacion = models.DateField()
+
+    def __str__(self):
+        return f"Venta {self.id} - {self.descripcion}"
