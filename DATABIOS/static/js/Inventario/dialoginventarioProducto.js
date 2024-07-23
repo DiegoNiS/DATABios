709caab7e5acbbba5735ac69c70d3ff3bba17ded
MaxsForocca <mforocca@unsa.edu.pe>
2024-07-23 13:08:34 -0500

    Dialog agregar producto basico

--- DATABIOS/static/js/Inventario/dialoginventarioProducto.js
+++ DATABIOS/static/js/Inventario/dialoginventarioProducto.js
@@ -1,33 +1,27 @@
 document.addEventListener('DOMContentLoaded', function() {
+    // Dialog Crear Productos
+    const DialogAdd = document.getElementById('DiaInvProAdd')
+    const openDialogAdd = document.getElementById('openDiaInvProAdd')
+    const closeDialogAdd = document.getElementById('closeDiaInvPraAdd')
+    const cancelDialogAdd = document.getElementById('cancelDiaInvProAdd') 
+    // Dialog Editar Productos
     const editarDialog = document.getElementById('editarProductoDialog');
+
+    // Eliminar Productos
     const eliminarDialog = document.getElementById('eliminarProductoDialog');
+
+    /* ADD dialog */
+    openDialogAdd.addEventListener('click', function() {
+        DialogAdd.showModal();
+    });
+
+    closeDialogAdd.addEventListener('click', function() {
+        DialogAdd.close();
+    });
+
+    cancelDialogAdd.addEventListener('click', function() {
+        DialogAdd.close();
+    });
     
-    document.querySelectorAll('.accion_producto').forEach(select => {
-        select.addEventListener('change', function(event) {
-            const productoId = event.target.getAttribute('data_id');
-            if (event.target.value === 'editar') {
-                // Abrir diálogo de edición
-                fetch(`/productos/detalle/${productoId}/`)
-                    .then(response => response.json())
-                    .then(data => {
-                        document.getElementById('editarNombreProducto').value = data.nombre;
-                        document.getElementById('editarStockProducto').value = data.stock;
-                        document.getElementById('editarPrecioCompraProducto').value = data.precio_compra;
-                        document.getElementById('editarPrecioVentaProducto').value = data.precio_venta;
-                        editarDialog.showModal();
-                    });
-            } else if (event.target.value === 'eliminar') {
-                // Abrir diálogo de eliminación
-                eliminarDialog.showModal();
-            }
-        });
-    });
-
-    document.getElementById('cancelarEditarProducto').addEventListener('click', function() {
-        editarDialog.close();
-    });
-
-    document.getElementById('cancelarEliminarProducto').addEventListener('click', function() {
-        eliminarDialog.close();
-    });
+    /* All delete Buttons */
 });
