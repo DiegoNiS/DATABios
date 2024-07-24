7914842baefef67c58d2c3e9ef541b325a7cc72e
GabSoto <gsotocco@unsa.edu.pe>
2024-07-23 21:33:10 -0500

    Vista de Gestión de proveedores.

--- DATABIOS/static/js/table.js
+++ DATABIOS/static/js/table.js
@@ -1,24 +1,39 @@
 $(document).ready(function() {
-    // Inicializar DataTables
-    const table = $('#myTable').DataTable({
-        "paging": true,     
-        "lengthChange": true,   
-        "searching": true,      
-        "ordering": true,   
-        "info": true,
-        "language": {
-            "url": "https://cdn.datatables.net/plug-ins/1.10.24/i18n/Spanish.json"
-        }
-    });
-
-    // Filtro personalizado para categorías
-    $('#categoryFilter').on('change', function() {
-        var selectedCategory = $(this).val();
-
-        if (selectedCategory) {
-            table.column(0).search('^' + selectedCategory + '$', true, false).draw();
+    try {
+        // Inicializar DataTables
+        const table = $('#myTable').DataTable({
+            "paging": true,     
+            "lengthChange": true,   
+            "searching": true,      
+            "ordering": true,   
+            "info": true,
+            "language": {
+                "url": "https://cdn.datatables.net/plug-ins/1.10.24/i18n/Spanish.json"
+            }
+        });
+    
+        // Filtro personalizado para categorías (lista_usuarios.html)
+        const categoryFilter = $('#categoryFilter');
+    
+        if (categoryFilter.length) { // Verifica si el elemento existe en el DOM
+            categoryFilter.on('change', function() {
+                var selectedCategory = $(this).val();
+    
+                try {
+                    if (selectedCategory) {
+                        table.column(0).search('^' + selectedCategory + '$', true, false).draw();
+                    } else {
+                        table.column(0).search('').draw();
+                    }
+                } catch (error) {
+                    console.error('Error al aplicar el filtro de categoría:', error);
+                }
+            });
         } else {
-            table.column(0).search('').draw();
+            console.warn('Filtrado no correspondiente.');
         }
-    });
+    } catch (error) {
+        console.error('Error al inicializar DataTables:', error);
+    }
+
 });
