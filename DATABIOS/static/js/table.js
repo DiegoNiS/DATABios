6206243a036a4d8072813453f69dc727bb744054
GabSoto <gsotocco@unsa.edu.pe>
2024-07-24 19:53:30 -0500

    Front end Lisatr productos.

--- DATABIOS/static/js/table.js
+++ DATABIOS/static/js/table.js
@@ -9,7 +9,7 @@ $(document).ready(function() {
             "info": true,
             "language": {
                 "url": "https://cdn.datatables.net/plug-ins/1.10.24/i18n/Spanish.json"
-            }
+            },
         });
     
         // Filtro personalizado para categorías (lista_usuarios.html)
@@ -57,4 +57,53 @@ $(document).ready(function() {
         console.error('Error al inicializar DataTables:', error);
     }
 
+    try {
+        // Inicializar DataTables
+        const tablePro = $('#tablePro').DataTable({
+            "paging": true,     
+            "lengthChange": true,   
+            "searching": true,      
+            "ordering": true,   
+            "info": true,
+            "language": {
+                "url": "https://cdn.datatables.net/plug-ins/1.10.24/i18n/Spanish.json"
+            },
+            columnDefs: [
+                {
+                    targets: 1,
+                    render: function(data, type, row) {
+                        if (type === 'display') {
+                            var listItems = $(data).find('li').map(function() {
+                                return $(this).text();
+                            }).get().join(', ');
+                            return listItems;
+                        }
+                        return data;
+                    }
+                }
+            ]
+        });
+        // Filtro personalizado para categorías (lista_usuarios.html)
+        const categoryFilterPro = $('#categoryFilterPro');
+    
+        if (categoryFilterPro.length) { // Verifica si el elemento existe en el DOM
+            categoryFilterPro.on('change', function() {
+                var selectedCategory = $(this).val();    
+                try {
+                    if (selectedCategory) {
+                        tablePro.column(1).search(selectedCategory, true, false).draw();
+                    } else {
+                        tablePro.column(1).search('').draw();
+                    }
+                } catch (error) {
+                    console.error('Error al aplicar el filtro de categoría:', error);
+                }
+            });
+        } else {
+            console.warn('Filtrado no corresponde a este DOM.');
+        }
+
+    } catch (error) {
+        console.error('Error al inicializar DataTables:', error);
+    }
 });
