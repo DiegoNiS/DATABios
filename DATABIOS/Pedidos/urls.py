8a448dc66be822aac243eddd5e811ad113d02be3
Omar <oromero@unsa.edu.pe>
2024-07-25 09:24:32 -0500

    Separando los pedidos entregados

--- DATABIOS/Pedidos/urls.py
+++ DATABIOS/Pedidos/urls.py
@@ -5,6 +5,5 @@ from . import views
 urlpatterns = [
     path('listar_pedidos/', views.listar_pedidos, name='listar_pedidos'),
     path('crear_pedidos/', views.crear_pedidos, name='crear_pedidos'),
-    #path('listar_proveedores/', views.listar_proveedores, name='listar_proveedores'),
-    #path('crear_proveedores/', views.crear_proveedores, name='crear_proveedores'),
+    path('listar_pedidos_entregados/', views.listar_pedidos_entregados, name='listar_pedidos_entregados'),
 ]
