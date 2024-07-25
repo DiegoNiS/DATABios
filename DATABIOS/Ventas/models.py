bbedbc224a82b8a85a6be298e301c715bb1d7a1a
Ower Frank <olopeza@unsa.edu.pe>
2024-07-25 02:12:35 -0500

    Avance de la logica de Ventas

--- DATABIOS/Ventas/models.py
+++ DATABIOS/Ventas/models.py
@@ -4,7 +4,7 @@ from Core.models import Usuario, Producto
 class Venta(models.Model):
     vendedor = models.ForeignKey(Usuario, on_delete=models.CASCADE)
     fecha_creacion = models.DateTimeField(auto_now_add=True)
-    total = models.DecimalField(max_digits=10, decimal_places=2, default=0)
+    total = models.DecimalField(max_digits=10, decimal_places=2)
 
     def __str__(self):
         return f"Venta {self.id}"
